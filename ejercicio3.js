79560f8599ce8856a82c9f2363e002b80f235f9d
Lisandro Coronel <lisandrojulian2004@gmail.com>
2023-04-03 01:22:51 -0300

    cambie el ejercicio 3

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,36 +1,18 @@
 class Rectangulo {
-    constructor(alto, ancho) {
-      this.alto = alto;
-      this.ancho = ancho;
-    }
-  
-    modificarAlto(nuevoAlto) {
-      this.alto = nuevoAlto;
-    }
-  
-    modificarAncho(nuevoAncho) {
-      this.ancho = nuevoAncho;
-    }
-  
-    mostrarPropiedades() {
-      console.log(`El rectángulo tiene un alto de ${this.alto} y un ancho de ${this.ancho}.`);
-    }
-  
-    calcularPerimetro() {
-      return 2 * (this.alto + this.ancho);
-    }
-  
-    calcularArea() {
-      return this.alto * this.ancho;
-    }
+  constructor(alto, ancho) {
+    this.alto = alto;
+    this.ancho = ancho;
   }
-  
-  
-  const miRectangulo = new Rectangulo(5, 10);
-  miRectangulo.mostrarPropiedades(); 
-  console.log(`El perímetro del rectángulo es ${miRectangulo.calcularPerimetro()} y su área es ${miRectangulo.calcularArea()}.`); 
-  
-  miRectangulo.modificarAlto(8);
-  miRectangulo.modificarAncho(12);
-  miRectangulo.mostrarPropiedades();
-  console.log(`El perímetro del rectángulo es ${miRectangulo.calcularPerimetro()} y su área es ${miRectangulo.calcularArea()}.`); 
+
+  mostrarPropiedades() {
+    console.log(`Alto: ${this.alto}, Ancho: ${this.ancho}`);
+  }
+
+  calcularPerimetro() {
+    return (this.alto + this.ancho) * 2;
+  }
+
+  calcularArea() {
+    return this.alto * this.ancho;
+  }
+}
